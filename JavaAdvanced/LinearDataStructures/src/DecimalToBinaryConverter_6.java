92ba4878904295ffa48bf88260e993199e42f9b9
Stefan Stoynov <sstoynov87@gmail.com>
2018-05-18 22:56:45 +0300

    added new solutions

--- JavaAdvanced/LinearDataStructures/src/DecimalToBinaryConverter_6.java
+++ JavaAdvanced/LinearDataStructures/src/DecimalToBinaryConverter_6.java
@@ -1,7 +1,23 @@
+import java.util.ArrayDeque;
 import java.util.Scanner;
 
 public class DecimalToBinaryConverter_6 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int num = Integer.parseInt(scanner.nextLine());
+
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+        if (num == 0) {
+            System.out.println(0);
+        } else {
+            while (num != 0) {
+                stack.push(num % 2);
+                num /= 2;
+            }
+        }
+        int iterations = stack.size();
+        for (int i = 0; i <iterations ; i++) {
+            System.out.print(stack.pop());
+        }
     }
 }
--- JavaAdvanced/LinearDataStructures/src/SumMatrixElements_3.java
+++ JavaAdvanced/LinearDataStructures/src/SumMatrixElements_3.java
@@ -1,7 +1,27 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class SumMatrixElements_3 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        int[] dimentions = Arrays.stream(scanner.nextLine().split(", ")).mapToInt(Integer::parseInt).toArray();
+
+        int rows = dimentions[0];
+        int cols = dimentions[1];
+
+        int[][] matrix = new int[rows][cols];
+        int sum = 0;
+        for (int i = 0; i < rows; i++) {
+            int[]reminder = Arrays.stream(scanner.nextLine().split(", ")).mapToInt(Integer::parseInt).toArray();
+            for (int j = 0; j <cols ; j++) {
+                matrix[i][j]=reminder[j];
+                sum += reminder[j];
+            }
+            System.out.println();
+        }
+        System.out.println(rows);
+        System.out.println(cols);
+        System.out.println(sum);
     }
 }
