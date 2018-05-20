c6021b2326ec993b9db1d527856f26f2b3d5eada
Stefan Stoynov <sstoynov87@gmail.com>
2018-05-20 19:12:08 +0300

    added new solutions

--- JavaAdvanced/LinearDataStructures/src/HotPotato_8.java
+++ JavaAdvanced/LinearDataStructures/src/HotPotato_8.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class HotPotato_8 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String children = scanner.nextLine();
+        String[] children = scanner.nextLine().split("\\s+");
 
         int count = Integer.parseInt(scanner.nextLine());
 
@@ -13,11 +13,12 @@ public class HotPotato_8 {
         Collections.addAll(queue, children);
 
         while (queue.size() > 1) {
-            for (int i = 0; i < queue.size(); i++) {
-                if (i == count){
-                    queue.poll();
-                }
+            for (int i = 1; i < count; i++) {
+                String firstChild = queue.poll();
+                queue.offer(firstChild);
             }
+            System.out.println("Removed " + queue.poll());
         }
+        System.out.println("Last is "+ queue.poll());
     }
 }
--- JavaAdvanced/LinearDataStructures/src/MathPotato_9.java
+++ JavaAdvanced/LinearDataStructures/src/MathPotato_9.java
@@ -1,7 +1,46 @@
+import java.util.ArrayDeque;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class MathPotato_9 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        String[] children = scanner.nextLine().split("\\s+");
+        int n = Integer.parseInt(scanner.nextLine());
+
+        ArrayDeque<String> queue = new ArrayDeque<>();
+        Collections.addAll(queue, children);
+        int counter = 1;
+        while (queue.size() > 1) {
+            for (int i = 1; i < n; i++) {
+
+                String firstChild = queue.poll();
+                queue.offer(firstChild);
+
+            }
+            if (isPrime(counter)) {
+                System.out.println("Prime " + queue.peek());
+            } else {
+                System.out.println("Removed " + queue.poll());
+            }
+            counter++;
+        }
+        System.out.println("Last is " + queue.poll());
+    }
+
+    public static boolean isPrime(int n) {
+
+        if (n <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
+
