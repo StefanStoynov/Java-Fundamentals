86d5775a086f0f733a944be6bb400c5d53b2299e
Stefan Stoynov <sstoynov87@gmail.com>
2018-05-20 13:36:41 +0300

    new solutions added

--- JavaAdvanced/LinearDataStructures/src/HotPotato_8.java
+++ JavaAdvanced/LinearDataStructures/src/HotPotato_8.java
@@ -1,7 +1,23 @@
+import java.util.ArrayDeque;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class HotPotato_8 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String children = scanner.nextLine();
+
+        int count = Integer.parseInt(scanner.nextLine());
+
+        ArrayDeque<String> queue = new ArrayDeque<>();
+        Collections.addAll(queue, children);
+
+        while (queue.size() > 1) {
+            for (int i = 0; i < queue.size(); i++) {
+                if (i == count){
+                    queue.poll();
+                }
+            }
+        }
     }
 }
