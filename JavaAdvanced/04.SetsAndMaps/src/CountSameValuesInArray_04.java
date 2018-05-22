37c7963e3293045f0dab37f5d5d059a737c276de
Stefan Stoynov <sstoynov87@gmail.com>
2018-05-22 10:56:20 +0300

    added CountSameValuesInArray_04

--- JavaAdvanced/04.SetsAndMaps/src/CountSameValuesInArray_04.java
+++ JavaAdvanced/04.SetsAndMaps/src/CountSameValuesInArray_04.java
@@ -1,2 +1,25 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class CountSameValuesInArray_04 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[]inputOfNumbers =scanner.nextLine().split("\\s+");
+
+        Map<String,Integer> numbersCount = new HashMap<>();
+
+        for (String number : inputOfNumbers) {
+            if (!numbersCount.containsKey(number)){
+                numbersCount.put(number,0);
+            }
+            numbersCount.put(number,numbersCount.get(number)+1);
+
+        }
+        for (Map.Entry<String, Integer> entryNumber : numbersCount.entrySet()) {
+            System.out.printf("%s - %s times%n",entryNumber.getKey(),entryNumber.getValue());
+        }
+    }
 }
