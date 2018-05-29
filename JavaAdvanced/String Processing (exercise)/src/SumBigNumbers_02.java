cd85cb1009e52b4806afdceaf73bd74cf8029337
Stefan Stoynov <sstoynov87@gmail.com>
2018-05-29 14:32:22 +0300

    new solution added

---
+++ JavaAdvanced/String Processing (exercise)/src/SumBigNumbers_02.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class SumBigNumbers_02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String firstNum = scanner.nextLine();
+        String secondNum = scanner.nextLine();
+
+        StringBuilder sb = new StringBuilder();
+
+        if (firstNum.length() > secondNum.length()) {
+        }
+    }
+}
