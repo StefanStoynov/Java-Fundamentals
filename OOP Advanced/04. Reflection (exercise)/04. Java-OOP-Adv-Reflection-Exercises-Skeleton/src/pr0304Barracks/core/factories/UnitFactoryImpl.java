67478dbcda4342d75c3028afe6d77c980017b14a
Stefan Stoynov <sstoynov87@gmail.com>
2018-07-23 11:12:05 +0300

    commit solution of 3rd task

--- OOP Advanced/04. Reflection (exercise)/04. Java-OOP-Adv-Reflection-Exercises-Skeleton/src/pr0304Barracks/core/factories/UnitFactoryImpl.java
+++ OOP Advanced/04. Reflection (exercise)/04. Java-OOP-Adv-Reflection-Exercises-Skeleton/src/pr0304Barracks/core/factories/UnitFactoryImpl.java
@@ -4,14 +4,26 @@ import jdk.jshell.spi.ExecutionControl;
 import pr0304Barracks.contracts.Unit;
 import pr0304Barracks.contracts.UnitFactory;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 public class UnitFactoryImpl implements UnitFactory {
 
-	private static final String UNITS_PACKAGE_NAME =
-			"pr0304Barracks.models.units.";
+    private static final String UNITS_PACKAGE_NAME =
+            "pr0304Barracks.models.units.";
 
-	@Override
-	public Unit createUnit(String unitType) throws ExecutionControl.NotImplementedException {
-		// TODO: implement for problem 3
-		throw new ExecutionControl.NotImplementedException("message");
-	}
+    @Override
+    public Unit createUnit(String unitType) throws ExecutionControl.NotImplementedException {
+        Unit unit = null;
+
+        try {
+            Class<?> unitClass = Class.forName(UNITS_PACKAGE_NAME + unitType);
+            Constructor<?> declaredConstructor = unitClass.getDeclaredConstructor();
+            unit = (Unit) declaredConstructor.newInstance();
+        } catch (ClassNotFoundException | NoSuchMethodException | IllegalAccessException | InstantiationException | InvocationTargetException e) {
+            e.printStackTrace();
+        }
+
+        return unit;
+    }
 }
