5d7a861890371af69300e6ff4ac9fe7c98db2444
Stefan Stoynov <sstoynov87@gmail.com>
2018-07-23 14:19:26 +0300

    adding solutions of first two tasks

---
+++ OOP Advanced/04. Reflection (lab)/src/p01_Reflection/Main.java
@@ -0,0 +1,22 @@
+package p01_Reflection;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.InvocationTargetException;
+
+public class Main {
+    public static void main(String[] args) throws IOException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        Class<?> reflection = Reflection.class;
+
+        System.out.println(reflection);
+        System.out.println(reflection.getSuperclass());
+        Class<?>[] interfaces = reflection.getInterfaces();
+        for (Class<?> anInterface : interfaces) {
+            System.out.println(anInterface);
+        }
+        Reflection ref = (Reflection) reflection.getDeclaredConstructor().newInstance();
+        System.out.println(ref);
+    }
+}
