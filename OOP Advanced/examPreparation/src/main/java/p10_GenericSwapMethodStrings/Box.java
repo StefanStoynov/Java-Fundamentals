2ad45015313dff3049d47e3a9bf0c1926bf34b00
Stefan Stoynov <sstoynov87@gmail.com>
2018-08-01 15:11:23 +0300

    commit solution of 10th task

---
+++ OOP Advanced/examPreparation/src/main/java/p10_GenericSwapMethodStrings/Box.java
@@ -0,0 +1,37 @@
+package p10_GenericSwapMethodStrings;
+
+
+import java.util.List;
+
+public class Box<T> {
+    private List<T> list;
+
+    public Box(List<T> list) {
+        this.list = list;
+    }
+
+    public List<T> getList() {
+        return this.list;
+    }
+
+    public void swap(int oldIndex, int newIndex) {
+        T element1 = this.list.get(oldIndex);
+        T element2 = this.list.get(newIndex);
+
+        this.list.remove(element1);
+        this.list.add(newIndex, element1);
+        this.list.remove(element2);
+        this.list.add(oldIndex, element2);
+
+
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (T t : list) {
+            sb.append(String.format("%s: %s", t.getClass().getName(), t)).append(System.lineSeparator());
+        }
+        return sb.toString();
+    }
+}
